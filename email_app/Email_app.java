a4ba18c86168b7df90071c629b69cf1c7195626e
Kevin Njogu <132701270+KevinNjogu@users.noreply.github.com>
2024-10-17 16:52:28 +0300

    Add files via upload
    This is an email generator application that helps the admin create new emails for new employees in the company.

---
+++ email_app/Email_app.java
@@ -0,0 +1,13 @@
+package email_app;
+
+
+public class Email_app {
+
+	
+	public static void main(String[] args) {
+		email eml = new email("Kevin", "Karongo");
+		System.out.println(eml.showInfo());
+		
+	}
+
+}
